718bfb853b292bbc43f09b26db785a7fc3149f1d
Felix Saaro <felix@saaro.ch>
2021-09-25 18:42:55 +0200

    Download from blob storage

--- GetInformation/index.js
+++ GetInformation/index.js
@@ -1,13 +1,48 @@
+const { BlobServiceClient } = require("@azure/storage-blob");
+
+const AZURE_STORAGE_CONNECTION_STRING =
+  process.env.NODE_ENV === "development"
+    ? process.env.AZURE_STORAGE_CONNECTION_STRING_HZ21
+    : process.env.AZURE_STORAGE_CONNECTION_STRING;
+
 module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
+  const blobServiceClient = BlobServiceClient.fromConnectionString(
+    AZURE_STORAGE_CONNECTION_STRING
+  );
+  const containerClient = blobServiceClient.getContainerClient("data");
+  const files = [];
+  for await (const blob of containerClient.listBlobsFlat()) {
+    files.push(blob.name);
+  }
 
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
-
+  const act = req.query.act;
+  if (!files.find((f) => f.startsWith(act))) {
     context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: responseMessage
+      status: 404,
+      body: { message: `Could not find file with ACT: ${act}` },
     };
+    return;
+  }
+
+  const blobClient = containerClient.getBlobClient(`${act}.json`);
+  const response = await blobClient.download();
+  const info = await streamToString(response.readableStreamBody);
+
+  context.res = {
+    status: 200,
+    body: info,
+  };
+};
+
+async function streamToString(readableStream) {
+  return new Promise((resolve, reject) => {
+    const chunks = [];
+    readableStream.on("data", (data) => {
+      chunks.push(data.toString());
+    });
+    readableStream.on("end", () => {
+      resolve(chunks.join(""));
+    });
+    readableStream.on("error", reject);
+  });
 }
